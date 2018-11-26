ce5f3cfc2b2f41240ce72821218f6bd20cad1e23
Abhishek Anand <anandabhinitp@gmail.com>
2018-11-27 01:07:39 +0530

    SideNave

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule, MatNativeDateModule, MatIconModule, MatSidenavModule, MatListModule, MatToolbarModule } from '@angular/material';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DataGridComponent } from './data-grid/data-grid.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SideBarComponent,
     DataGridComponent
   ],
   imports: [
@@ -18,7 +21,14 @@ import { DataGridComponent } from './data-grid/data-grid.component';
     AppRoutingModule,
     MatTableModule,
     MatSortModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
